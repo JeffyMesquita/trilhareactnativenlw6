59f8de1d45a77df139d7656009f90931ca7c1bf0
Jeferson Mesquita <je_2742@hotmail.com>
2021-06-28 13:18:13 -0300

    Aula 5 - Surface Exploration - 1:01:10
    Ajustes Guild

--- gameplay/src/components/GuildIcon/index.tsx
+++ gameplay/src/components/GuildIcon/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 import { styles } from './styles';
+import DiscordSVG from '../../assets/discord.svg';
 
-export function GuildIcon() {
-   const uri = 'https://images-na.ssl-images-amazon.com/images/I/51lpm9SpsJL.png'
+const { CDN_IMAGE } = process.env;
 
+type Props = {
+   guildId: string;
+   iconId: string | null;
+}
+
+export function GuildIcon({ guildId, iconId }: Props) {
+   const uri = `${CDN_IMAGE}icons/${guildId}/${iconId}.png`;
+
+   
    return (
-      <Image 
-         source={{ uri }}
-         style={styles.image}
-         resizeMode="cover"
-      />
+      <View style={styles.container}>
+         {
+            iconId ?
+            <Image
+               source={{ uri }}
+               style={styles.image}
+               resizeMode="cover"
+            />
+            :
+            <DiscordSVG
+               width={40}
+               height={40}               
+            />
+         }
+      </View>      
    )
 }
--- gameplay/src/components/GuildIcon/styles.ts
+++ gameplay/src/components/GuildIcon/styles.ts
@@ -1,9 +1,18 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
    image: {
       height: 80,
       width: 68,
-      borderRadius: 8,      
+      borderRadius: 8, 
+      backgroundColor: theme.colors.discord,
+      alignItems: 'center',
+      justifyContent: 'center',
+      overflow: 'hidden',
+   },
+   container: {
+      height: 80,
+      width: 68,       
    },
 });
